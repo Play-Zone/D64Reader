645dd4a781500e9b2bd624c6ffe444eca93aa79c
Damian <d@c64.ch>
2015-08-31 21:19:48 +0200

    Initial Commit

---
+++ DirectoryItem.cs
@@ -0,0 +1,12 @@
+﻿namespace d64Reader
+{
+    public class DirectoryItem
+    {
+        public int Blocks { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int FileStartingTrack { get; set; }
+        public int FileStartingSector { get; set; }
+        public bool IsOpen { get; set; }
+    }
+}
---
+++ DiskImage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace d64Reader
+{
+    public class DiskImage
+    {
+        public string DiskType { get; set; }
+        public List<DirectoryItem> DirectoryItems { get; set; }
+        public string DiskName { get; set; }
+        public string DiskId { get; set; }
+        public int FreeBlocks { get; set; }         
+    }
+}
